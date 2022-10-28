6724a8d12c12bc901ea3aadd47d1fd4134517848
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-10-28 19:49:52 -0300

    :tada: init: Configura base do projeto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+// using GoBarber.Models;
+
+namespace GoBarber.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
